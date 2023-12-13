8533aa3a3693a88d6fcd209949f39511182cde82
AmarnathaGowda <amarnath1413@gmail.com>
2023-12-13 13:14:53 +0530

    aiogram 3.2.0 updates

--- research/tele_echo_bot.py
+++ research/tele_echo_bot.py
@@ -1,26 +1,55 @@
 import logging
-from aiogram import Bot,Dispatcher,executor,types
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+# from aiogram.utils.markdown import hboldfrom 
 from dotenv import load_dotenv
 import os
+import asyncio
 
 load_dotenv()
 API_TOKEN = os.getenv("TOKEN")
-logging(API_TOKEN)
+print(API_TOKEN)
 
-#configuretion logging
-logging.basicConfig(level=logging.INFO)
+#configure logging
+# logging.basicConfig(level=logging.INFO)
 
-#Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+# Initialize bot and dispatcher
+# bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
 
-@dp.message_handler(commands=['start','help'])
-async def command_start_handler(message: Message) -> None:
+
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: types.Message) -> None:
     """
-    This handler receives messages with `/start`  or '/help'command
+    This handler receives messages with `/start` or  `/help `command
     """
-    
-    await message.reply(f"Hello\n I am Hammer Bot ready for scrapping !!")
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    This will retrun echo
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(API_TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    # logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
+    
